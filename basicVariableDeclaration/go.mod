9035c36c0cee09fcac8cfbe5f54545b1e38a22d9
Vikrant Chaudhary <vikrantachaudhary96@gmail.com>
2024-03-21 09:33:53 +0530

    variable

---
+++ basicVariableDeclaration/go.mod
@@ -0,0 +1,3 @@
+module variabledeclaration
+
+go 1.22.1
---
+++ basicVariableDeclaration/variable.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var rollno int = 23
+	fmt.Printf("my roll no is  no is %d ", rollno)
+	
+
+}
